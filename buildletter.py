30d85b5adb7c3e4c0268daa22bc1bd327426f026
cadenforrest <cadenforrest@gmail.com>
2022-01-27 11:50:22 -0600

    fixes

--- buildletter.py
+++ buildletter.py
@@ -1,88 +1,135 @@
 #!/usr/bin/python
 
 import csv
+from email.policy import default
 import os
 import pystache
 import sys
 
 
-names = csv.reader(open(sys.argv[1], "rU"))
-regtmpl = str(open("reg.mustache", "r").read())
-gifttmpl = str(open("gift.mustache", "r").read())
-
-count = 0
-gc = 0
-stop = 0
-
-csvname = sys.argv[1].split(".")[0]
-
-
 def remove_non_ascii(text):
     return text.encode("ascii", "ignore").decode()
 
+def build_letters(file = None): 
 
-if not os.path.exists("generated_pdfs/"):
-    os.makedirs("generated_pdfs/")
+    if file is None: 
+        names = csv.reader(open(sys.argv[1], "rU"))
+        csvname = sys.argv[1].split(".")[0]
+        folder_name = file.replace(".csv", "")
+    else: 
+        names = csv.reader(open("csv/" + file, "rU"))
+        csvname = file.split(".")[0]
 
-for row in names:
-    if count > stop:
-        toname = ""
-        fromname = ""
-        message = ""
-        size = "M"
-        gender = "M"
-        print(row)
-        print(len(row))
-        if str(row[4]) != "null":
-            toname = remove_non_ascii(str(row[4]))
-        if str(row[21]) != "null" and str(row[21]) != "#N/A" and str(row[21]) != "0":
-            fromname = remove_non_ascii(str(row[21]))
-        if str(row[22]) != "null" and str(row[22]) != "#N/A" and str(row[22]) != "0":
-            message = remove_non_ascii(str(row[22]))
-        if row[18] == "small":
-            size = "S"
-        if row[18] == "large":
-            size = "L"
-        if row[18] == "youth":
-            size = "Y"
+    regtmpl = str(open("reg.mustache", "r").read())
+    gifttmpl = str(open("gift.mustache", "r").read())
 
-        if row[19] == "adult_female":
-            gender = "F"
-        if row[19] == "kid_female":
-            gender = "F"
+    count = 0
+    gc = 0
+    stop = 0
 
-        filename = "{0:05d}".format(count)
-        context = {
-            "ToName": toname,
-            "FromName": fromname,
-            "MESSAGE": message,
-            "Size": size,
-            "Gender": gender,
-        }
-        if message:
-            giftout = str(pystache.render(gifttmpl, context))
-            f = open(("htmls/" + filename + ".html"), "w")
-            f.write(giftout)
-            f.close()
-            cmd = (
-                "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
-                + filename
-                + ".html generated_pdfs/"
-                + filename
-                + ".pdf"
-            )
-            os.system(cmd)
-        else:
-            regout = str(pystache.render(regtmpl, context))
-            f = open(("htmls/" + filename + ".html"), "w")
-            f.write(regout)
-            f.close()
-            cmd = (
-                "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
-                + filename
-                + ".html generated_pdfs/"
-                + filename
-                + ".pdf"
-            )
-            os.system(cmd)
-    count = count + 1
+    if not os.path.exists("generated_pdfs/"):
+        os.makedirs("generated_pdfs/")
+
+    if not os.path.exists("htmls/"): 
+        os.makedirs("htmls/")
+
+    for row in names:
+        if count > stop:
+            toname = ""
+            fromname = ""
+            message = ""
+            size = "M"
+            gender = "M"
+            print(row)
+            print(len(row))
+            if str(row[4]) != "null":
+                toname = remove_non_ascii(str(row[4]))
+            if str(row[21]) != "null" and str(row[21]) != "#N/A" and str(row[21]) != "0":
+                fromname = remove_non_ascii(str(row[21]))
+            if str(row[22]) != "null" and str(row[22]) != "#N/A" and str(row[22]) != "0":
+                message = remove_non_ascii(str(row[22]))
+            if row[18] == "small":
+                size = "S"
+            if row[18] == "large":
+                size = "L"
+            if row[18] == "youth":
+                size = "Y"
+
+            if row[19] == "adult_female":
+                gender = "F"
+            if row[19] == "kid_female":
+                gender = "F"
+
+            filename = "{0:05d}".format(count)
+            context = {
+                "ToName": toname,
+                "FromName": fromname,
+                "MESSAGE": message,
+                "Size": size,
+                "Gender": gender,
+            }
+            if message:
+                giftout = str(pystache.render(gifttmpl, context))
+                f = open(("htmls/" + filename + ".html"), "w")
+                f.write(giftout)
+                f.close()
+                cmd = (
+                    "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
+                    + filename
+                    + ".html generated_pdfs/"
+                    + filename
+                    + ".pdf"
+                )
+                os.system(cmd)
+            else:
+                regout = str(pystache.render(regtmpl, context))
+                f = open(("htmls/" + filename + ".html"), "w")
+                f.write(regout)
+                f.close()
+                cmd = (
+                    "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
+                    + filename
+                    + ".html generated_pdfs/"
+                    + filename
+                    + ".pdf"
+                )
+                os.system(cmd)
+        count = count + 1
+
+def merge_pdfs(pdf_name): 
+    from PyPDF2 import PdfFileMerger, PdfFileReader
+    merger = PdfFileMerger()
+    for file in os.listdir("generated_pdfs/"):
+        print(file)
+        if file.endswith(".pdf") and os.path.exists("generated_pdfs/" + file):
+            input = PdfFileReader(open("generated_pdfs/" + file, "rb"))
+            merger.append(input, import_bookmarks=False)
+    if not os.path.exists("merged_pdfs/"):
+        os.makedirs("merged_pdfs/")
+    merger.write(open("merged_pdfs/" + pdf_name + ".pdf", "wb"))
+    print("Merged PDFs at " + pdf_name + ".pdf")
+
+def remove_junk(): 
+    for file in os.listdir("generated_pdfs/"):
+        if file.endswith(".pdf") and file.startswith("0"):
+            os.remove("generated_pdfs/" + file)
+
+    for file in os.listdir("htmls/"):
+        if file.endswith(".html") and file.startswith("0"):
+            os.remove("htmls/" + file)
+
+if __name__ == "__main__":
+    import argparse
+    from split_csv import split 
+    argparser = argparse.ArgumentParser(description="Build letters")
+    argparser.add_argument("--filepath", type=str, help="Path to csv file")
+    args = argparser.parse_args()
+    if args.filepath:
+        split(args.filepath)
+        for file in os.listdir("csv/"):
+            if file.endswith(".csv"):
+                build_letters(file)
+                merge_pdfs(file.replace(".csv", ""))
+                remove_junk()
+    else:
+       "Didn't get a csv file path. Use --filepath to specify a csv file. Ex: 'python buildletter.py --name=example.csv'" 
--- split_csv.py
+++ split_csv.py
@@ -1,5 +1,7 @@
 import csv
+import os
 import sys
+
 
 def update_row(count, dict):
     """
@@ -25,41 +27,41 @@ def parse_dict(dict_list):
     takes a list of dictionaries and sorts them into lists by order type
     """
     FL_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FL_INTL[0]["size_code"] = 'FL'
-    FL_INTL[0]["country_code"] = 'INTL'
+    FL_INTL[0]["size_code"] = "FL"
+    FL_INTL[0]["country_code"] = "INTL"
     FL_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FL_US[0]["size_code"] = 'FL'
-    FL_US[0]["country_code"] = 'US'
+    FL_US[0]["size_code"] = "FL"
+    FL_US[0]["country_code"] = "US"
     FM_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FM_INTL[0]["size_code"] = 'FM'
-    FM_INTL[0]["country_code"] = 'INTL'
+    FM_INTL[0]["size_code"] = "FM"
+    FM_INTL[0]["country_code"] = "INTL"
     FM_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FM_US[0]["size_code"] = 'FM'
-    FM_US[0]["country_code"] = 'US'
+    FM_US[0]["size_code"] = "FM"
+    FM_US[0]["country_code"] = "US"
     FS_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FS_INTL[0]["size_code"] = 'FS'
-    FS_INTL[0]["country_code"] = 'INTL'
+    FS_INTL[0]["size_code"] = "FS"
+    FS_INTL[0]["country_code"] = "INTL"
     FS_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    FS_US[0]["size_code"] = 'FS'
-    FS_US[0]["country_code"] = 'US'
+    FS_US[0]["size_code"] = "FS"
+    FS_US[0]["country_code"] = "US"
     ML_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    ML_INTL[0]["size_code"] = 'ML'
-    ML_INTL[0]["country_code"] = 'INTL'
+    ML_INTL[0]["size_code"] = "ML"
+    ML_INTL[0]["country_code"] = "INTL"
     ML_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    ML_US[0]["size_code"] = 'ML'
-    ML_US[0]["country_code"] = 'US'
+    ML_US[0]["size_code"] = "ML"
+    ML_US[0]["country_code"] = "US"
     MM_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    MM_INTL[0]["size_code"] = 'MM'
-    MM_INTL[0]["country_code"] = 'INTL'
+    MM_INTL[0]["size_code"] = "MM"
+    MM_INTL[0]["country_code"] = "INTL"
     MM_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    MM_US[0]["size_code"] = 'MM'
-    MM_US[0]["country_code"] = 'US'
+    MM_US[0]["size_code"] = "MM"
+    MM_US[0]["country_code"] = "US"
     MS_INTL = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    MS_INTL[0]["size_code"] = 'MS'
-    MS_INTL[0]["country_code"] = 'INTL'
+    MS_INTL[0]["size_code"] = "MS"
+    MS_INTL[0]["country_code"] = "INTL"
     MS_US = [dict.fromkeys(["start_row", "end_row", "size_code", "country_code"])]
-    MS_US[0]["size_code"] = 'MS'
-    MS_US[0]["country_code"] = 'US'
+    MS_US[0]["size_code"] = "MS"
+    MS_US[0]["country_code"] = "US"
 
     row_num = 0
     for row in dict_list:
@@ -138,24 +140,40 @@ def make_csv(dict_list):
     """
     Takes a list of dictionaries and writes them to a CSV file
     """
-
     column_names = dict_list[1].keys()
-    csv_name = "{size_code}_{first_row}_{last_row}_{country_code}.csv".format(
+    csv_name = "csv/{size_code}_{first_row}_{last_row}_{country_code}.csv".format(
         size_code=dict_list[0]["size_code"],
         first_row=dict_list[0]["start_row"],
         last_row=dict_list[0]["end_row"],
         country_code=dict_list[0]["country_code"],
     )
-    print(csv_name)
+    if not os.path.exists("csv/"):
+        os.mkdir("csv/")
+
     with open(csv_name, "w") as outfile:
         writer = csv.DictWriter(outfile, fieldnames=column_names)
         writer.writeheader()
         for row in dict_list[1:]:
             writer.writerow(row)
+    print(csv_name)
 
-if __name__ == "__main__":
-    csv_dict = parse_csv_to_dict(sys.argv[1])
-    FL_INTL, FL_US, FM_INTL, FM_US, FS_INTL, FS_US, ML_INTL, ML_US, MM_INTL, MM_US, MS_INTL, MS_US = parse_dict(csv_dict)
+def split(csv_name): 
+    csv_dict = parse_csv_to_dict(csv_name)
+    (
+        FL_INTL,
+        FL_US,
+        FM_INTL,
+        FM_US,
+        FS_INTL,
+        FS_US,
+        ML_INTL,
+        ML_US,
+        MM_INTL,
+        MM_US,
+        MS_INTL,
+        MS_US,
+    ) = parse_dict(csv_dict)
+    print('Created CSVs: ')
     make_csv(FL_INTL)
     make_csv(FL_US)
     make_csv(FM_INTL)
@@ -168,3 +186,6 @@ if __name__ == "__main__":
     make_csv(MM_US)
     make_csv(MS_INTL)
     make_csv(MS_US)
+
+if __name__ == "__main__":
+    split(sys.argv[1])
