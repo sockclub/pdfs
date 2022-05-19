1a0d0ad2222c02aed8d1215814e327e8fc971498
cadenforrest <cadenforrest@gmail.com>
2022-05-19 13:31:33 -0500

    alphabetize

--- buildletter.py
+++ buildletter.py
@@ -1,103 +1,111 @@
 #!/usr/bin/python
-
 import csv
+from doctest import master
 from email.policy import default
 import os
 import pystache
 import sys
+from split_csv import parse_csv_to_dict
+import inflection 
 
 
 def remove_non_ascii(text):
     return text.encode("ascii", "ignore").decode()
 
-def build_letters(file = None): 
-
-    if file is None: 
-        names = csv.reader(open(sys.argv[1], "rU"))
-        csvname = sys.argv[1].split(".")[0]
-        folder_name = file.replace(".csv", "")
-    else: 
-        names = csv.reader(open("csv/" + file, "rU"))
-        csvname = file.split(".")[0]
-
+def builder(file=None):
+    csv_dict = parse_csv_to_dict(file)
     regtmpl = str(open("reg.mustache", "r").read())
     gifttmpl = str(open("gift.mustache", "r").read())
-
-    count = 0
-    gc = 0
-    stop = 0
-
     if not os.path.exists("generated_pdfs/"):
         os.makedirs("generated_pdfs/")
-
-    if not os.path.exists("htmls/"): 
+    if not os.path.exists("htmls/"):
         os.makedirs("htmls/")
 
-    for row in names:
-        if count > stop:
-            toname = ""
-            fromname = ""
-            message = ""
+    for row in csv_dict:
+        to_name = row["Recipient name"]
+        from_name = row["Fromname"]
+        message = row["Message"]
+
+        # map the size to the correct size code
+        if "small" in row["Size"]:
+            size = "S"
+        elif "large" in row["Size"]:
+            size = "L"
+        elif "youth" in row["Size"]:
+            size = "Y"
+        elif "medium" in row["Size"]:
             size = "M"
+        elif "Medium" in row["Size"]:
+            size = "M"
+        else:
+            raise Exception("Invalid size for recipient " + row["Recipient name"])
+
+        # map the style/gender
+        if row["Sub type"] == "adult_female":
+            gender = "F"
+        elif row["Sub type"] == "kid_female":
+            gender = "F"
+        elif row["Sub type"] == "adult_male": 
             gender = "M"
-            print(row)
-            print(len(row))
-            if str(row[4]) != "null":
-                toname = remove_non_ascii(str(row[4]))
-            if str(row[21]) != "null" and str(row[21]) != "#N/A" and str(row[21]) != "0":
-                fromname = remove_non_ascii(str(row[21]))
-            if str(row[22]) != "null" and str(row[22]) != "#N/A" and str(row[22]) != "0":
-                message = remove_non_ascii(str(row[22]))
-            if row[18] == "small":
-                size = "S"
-            if row[18] == "large":
-                size = "L"
-            if row[18] == "youth":
-                size = "Y"
+        elif row["Sub type"] == "kid_male": 
+            gender = "M"
 
-            if row[19] == "adult_female":
-                gender = "F"
-            if row[19] == "kid_female":
-                gender = "F"
+        # if the message is blank, generate regular html doc with no gift message
+        html_str = str(
+            pystache.render(
+                regtmpl if message == "" else gifttmpl,
+                {
+                    "ToName": to_name,
+                    "FromName": from_name,
+                    "MESSAGE": message,
+                    "Size": size,
+                    "Gender": gender,
+                },
+            )
+        )
+        master_file_name = file.replace(".csv","").replace("csv/", "")
+        print(master_file_name)
+        append_html_string_to_pdf(html_str, inflection.parameterize(to_name), master_file_name)
+    
 
-            filename = "{0:05d}".format(count)
-            context = {
-                "ToName": toname,
-                "FromName": fromname,
-                "MESSAGE": message,
-                "Size": size,
-                "Gender": gender,
-            }
-            if message:
-                giftout = str(pystache.render(gifttmpl, context))
-                f = open(("htmls/" + filename + ".html"), "w")
-                f.write(giftout)
-                f.close()
-                cmd = (
-                    "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
-                    + filename
-                    + ".html generated_pdfs/"
-                    + filename
-                    + ".pdf"
-                )
-                os.system(cmd)
-            else:
-                regout = str(pystache.render(regtmpl, context))
-                f = open(("htmls/" + filename + ".html"), "w")
-                f.write(regout)
-                f.close()
-                cmd = (
-                    "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
-                    + filename
-                    + ".html generated_pdfs/"
-                    + filename
-                    + ".pdf"
-                )
-                os.system(cmd)
-        count = count + 1
-
-def merge_pdfs(pdf_name): 
+def append_html_string_to_pdf(html_str, temp_file_name, master_file_name="master"):
     from PyPDF2 import PdfFileMerger, PdfFileReader
+    # write the html to a file
+    f = open(("htmls/" + temp_file_name + ".html"), "w")
+    f.write(html_str)
+    f.close()
+
+    # if the master pdf doesn't exist yet, create it and return 
+    if not os.path.exists("merged_pdfs/" + master_file_name + ".pdf"):
+        os.system(
+            "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
+            + temp_file_name
+            + ".html merged_pdfs/"
+            + master_file_name
+            + ".pdf"
+        )
+        return 
+
+    # if the master pdf exists, make a new pdf from the html 
+    os.system(
+        "/usr/local/bin/wkhtmltopdf --enable-local-file-access htmls/"
+        + temp_file_name
+        + ".html generated_pdfs/"
+        + temp_file_name
+        + ".pdf"
+    )
+
+    # merge the new pdf with the master pdf
+    merger = PdfFileMerger()
+    merger.append(PdfFileReader(open("merged_pdfs/" + master_file_name + ".pdf", "rb")), import_bookmarks=False)
+    merger.append(PdfFileReader(open("generated_pdfs/" + temp_file_name + ".pdf", "rb")), import_bookmarks=False)
+    merger.write(open("merged_pdfs/" + master_file_name + ".pdf", "wb"))
+    print("Appended " + temp_file_name + " to merged_pdfs/" + master_file_name + ".pdf")
+
+
+def merge_pdfs(pdf_name):
+    from PyPDF2 import PdfFileMerger, PdfFileReader
+
     merger = PdfFileMerger()
     for file in os.listdir("generated_pdfs/"):
         if file.endswith(".pdf") and os.path.exists("generated_pdfs/" + file):
@@ -109,22 +117,25 @@ def merge_pdfs(pdf_name):
     print("Merged PDFs at " + pdf_name + ".pdf")
     remove_blank_pages(pdf_name)
 
+
 def remove_blank_pages(pdf_name):
-    from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+    from PyPDF2 import PdfFileReader, PdfFileWriter
     import pdfplumber
+
     pdfReader = PdfFileReader(open("merged_pdfs/" + pdf_name + ".pdf", "rb"))
     PdfFileWriter = PdfFileWriter()
     with pdfplumber.open("merged_pdfs/" + pdf_name + ".pdf") as plumber:
         for page in range(pdfReader.getNumPages()):
-            if len(plumber.pages[page].chars) > 0: 
+            if len(plumber.pages[page].chars) > 0:
                 PdfFileWriter.addPage(pdfReader.getPage(page))
-                print('added page')
+                print("added page")
     if not os.path.exists("final_pdfs/"):
         os.makedirs("final_pdfs/")
     PdfFileWriter.write(open("final_pdfs/" + pdf_name + ".pdf", "wb"))
     print("Removed blank pages at " + pdf_name + ".pdf")
 
-def remove_pdfs(): 
+
+def remove_pdfs():
     for file in os.listdir("generated_pdfs/"):
         if file.endswith(".pdf") and file.startswith("0"):
             os.remove("generated_pdfs/" + file)
@@ -133,9 +144,11 @@ def remove_pdfs():
         if file.endswith(".html") and file.startswith("0"):
             os.remove("htmls/" + file)
 
+
 if __name__ == "__main__":
     import argparse
-    from split_csv import split 
+    from split_csv import split
+
     argparser = argparse.ArgumentParser(description="Build letters")
     argparser.add_argument("--filepath", type=str, help="Path to csv file")
     args = argparser.parse_args()
@@ -143,9 +156,9 @@ if __name__ == "__main__":
         split(args.filepath)
         for file in os.listdir("csv/"):
             if file.endswith(".csv"):
-                build_letters(file)
-                merge_pdfs(file.replace(".csv", ""))
-                remove_pdfs()
+              builder("csv/" + file)
+              remove_blank_pages(file.replace(".csv",""))
+              remove_pdfs()
 
     else:
-       "Didn't get a csv file path. Use --filepath to specify a csv file. Ex: 'python buildletter.py --name=example.csv'" 
+        "Didn't get a csv file path. Use --filepath to specify a csv file. Ex: 'python buildletter.py --name=example.csv'"
